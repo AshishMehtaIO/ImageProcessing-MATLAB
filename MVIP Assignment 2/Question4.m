a70e5117d587047c043565220fbe3189aadf8c30
Ashish Mehta <ashish.biren@learner.manipal.edu>
2017-03-24 09:41:58 +0530

    Add files via upload

---
+++ MVIP Assignment 2/Question4.m
@@ -0,0 +1,16 @@
+%Qestion 4 - DFT
+clear all;
+clc;
+
+im1=zeros(512);
+im1(240:272,240:272)=255;
+imshow(uint8(im1));
+
+im_ff=abs(fft2(im1));
+figure(1)
+imshow(uint8(im_ff));
+figure(2)
+im_fft_shift=fftshift(im_ff);
+imshow(uint8(im_fft_shift));
+
+%Similar code for other images
